77d1668561c7510a10e4c9a596ba2d298d1f8f2f
RenatoNappo <31985727+RenatoNappo@users.noreply.github.com>
2018-06-08 10:34:39 +0100

    Section 6 - Web API enabled CustomersController & MoviesController Controllers added to API folder.

---
+++ Vidly2/Controllers/API/CustomersController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        // GET /api/customers
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+
+
+        // GET /api/customer/{id}
+        [HttpGet]
+        public Customer GetCUstomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        
+        // POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+
+        // PUT api/customers/{id, customer}
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.BirthDate = customer.BirthDate;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+
+            _context.SaveChanges();
+        }
+
+
+        // DELETE api/customers/{id}
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerToDelete = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerToDelete == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerToDelete);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Vidly2/Controllers/API/MoviesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers.API
+{
+    public class MoviesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        // GET api/movies
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+
+        // GET api/movies/{id}
+        [HttpGet]
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return movie;
+        }
+
+        
+        // POST api/movies
+        [HttpPost]
+        public Movie CreateMovie(Movie movie)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return movie;
+        }
+
+
+        // PUT api/movies/{id}
+        [HttpPut]
+        public void UpdateMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            movieInDb.Name = movie.Name;
+            movieInDb.DateAdded = movie.DateAdded;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.GenreTypeId = movie.GenreTypeId;
+            movieInDb.NumberInStock = movie.NumberInStock;
+
+            _context.SaveChanges();
+        }
+
+        
+        // DELETE api/movies{id}
+        [HttpDelete]
+        public void DeleteMovie(int id)
+        {
+            var movieToRemove = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieToRemove == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(movieToRemove);
+            _context.SaveChanges();
+        }
+
+    }
+}
